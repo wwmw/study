493fdfca8f0677830387e2f014a42f2484688927
wwmwHP <wwmw07@gmail.com>
2022-04-28 12:25:12 +0900

    no message

---
+++ ServletFormTest/src/com/servlet/MemSignUp.java
@@ -0,0 +1,45 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/mSignUp")
+public class MemSignUp extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("-- doGet() --");
+		
+		String m_name = request.getParameter("m_name");
+		String m_pass = request.getParameter("m_pass");
+		String m_gender = request.getParameter("m_gender");
+		String[] m_hobby = request.getParameterValues("m_hobby");
+		String m_residence = request.getParameter("m_residence");
+		
+		System.out.println("m_name : " + m_name);
+		System.out.println("m_pass : " + m_pass);
+		System.out.println("m_gender : " + m_gender);
+		System.out.println("m_hobby : " + Arrays.toString(m_hobby));
+		System.out.println("m_residence : " + m_residence);
+		
+		Enumeration<String> names = request.getParameterNames();
+		while(names.hasMoreElements()) {
+			String name = (String)names.nextElement();
+			System.out.println("name : " + name);
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		
+		System.out.println("-- doPost() --");
+	}
+
+}
